b07279243914050a34679e13505ee89d054d496d
Alfredo <alfredomartinezruiz01@gmail.com>
2021-05-23 13:53:00 -0500

    Bug en teclas

--- Editor.c
+++ Editor.c
@@ -1,1 +1,62 @@
 /* Interfaz editor*/
+#include "FuncEditor.c"
+
+void finalizar(int snum){
+		exit(0);
+}
+
+int abrirArchivo (char *direccion){
+    crearVentana();
+    mostrarHeaderFooter();
+    posX = 5;
+    posY = 5;
+    /*Compartiendo las coordeanadas del mono entre los procesos*/
+	int *xM = shmat(shmget(ftok("Prac8",'k'), sizeof(int),IPC_CREAT|0600), 0, 0);
+	int *yM = shmat(shmget(ftok("Prac8",'k'), sizeof(int),IPC_CREAT|0600), 0, 0);
+
+    pid_t pid;
+	pid = fork();
+	if(pid == 0){ // Proceso encargado de dibujar 
+		*xM = posX;
+		*yM = posY;
+		while (1){
+			posX = *xM;	
+            //posY = *yM;	
+		}		
+	exit(0);	
+	}
+	
+	else{ // Proceso encargado de detectar movimiento del cursor y el tipeado
+		while(1){
+            
+			//noecho();        
+    		cbreak();
+    		signal(SIGTERM, finalizar);
+			int key = getch();
+			
+			if(key == IZQUIERDA){	//Si presiona la tecla "a"
+				posX = *xM;
+				mover(-2);
+			}
+			else if(key == DERECHA){	//Si presiona la tecla "d"
+				posX = *xM;
+				mover(2);
+			}
+			else{	//Si no se mueve a la derecha o izquierda imprime lo siguiente, esto se puede cambiar para que no imprima nada o no se mueva
+					//Lo hice para ver que el movimiento se haga correctamente
+				printw(".");
+			}
+            //printw("%d + %d", posX, posY);
+			*xM = posX;
+
+		}			
+	}
+    return 0;
+}
+int main(){
+    abrirArchivo("sd");   
+
+    getch();         
+    endwin();         
+    return 0; 
+}
--- FuncEditor.c
+++ FuncEditor.c
@@ -1,1 +1,98 @@
 /* Funciones que usa el editor */
+# include <ncurses.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h> 
+# include <unistd.h> 
+# include <time.h>
+# include<sys/ipc.h>
+# include<sys/shm.h>
+#include <signal.h> 
+
+/* Teclas para controlar al mono*/
+# define ESCAPE 27
+# define ENTER 10
+# define IZQUIERDA 97
+# define DERECHA 100
+# define ARRIBA 119
+# define ABAJO 115
+
+/* Declaracion de las vars principales */
+WINDOW *Editor;  
+
+/* Para obtener tamaño de ventanas*/
+int maxX;
+int maxY;
+
+/* Variables para controlar al cursor*/
+int posX, posY; 
+
+/* c�digo implementaci�n de todas las funciones */
+void crearVentana(){
+    Editor = initscr(); // Crea la ventana
+    //inicializacion de colores
+    if(has_colors())
+	    start_color();
+
+    init_pair(1,COLOR_BLACK, COLOR_WHITE); //texto
+    init_pair(2,COLOR_BLUE,COLOR_WHITE); //mono
+    init_pair(3,COLOR_MAGENTA, COLOR_MAGENTA); //cubos
+    bkgd(COLOR_PAIR(1));
+    attron(COLOR_PAIR(1));
+
+    // Obtenemos resolución en espacios y renglones
+	maxY = getmaxy(Editor);
+	maxX = getmaxx(Editor); 
+    posX = (maxX/2)-2;
+    posY = maxY-6;
+}
+
+/* Función encargada de mostrar el header y el footer del editor*/
+int mostrarHeaderFooter(){
+    clear();
+    attron(A_BOLD);
+    //attron(A_BLINK);
+
+    /* Mostrando titulo*/
+    move(0, maxX/2-5);
+    printw("Editron");
+
+    for(int i = 0; i<maxX; i++){
+        move(1, i);
+        printw("-");
+    }
+    
+    /* Mostrando footer*/
+    for(int i = 0; i<maxX; i++){
+        move(maxY-4, i);
+        printw("-");
+    }
+
+    move(maxY-3, 1);
+    printw("Ctrl + g para guardar");
+    move(maxY-2, 1);
+    printw("Ctrl + o para volver a menu");
+
+    move(maxY-3, 30);
+    printw("Ctrl + c para para salir");
+    move(maxY-2, 30);
+    printw("Ctrl + o para volver a menu");
+
+    refresh();
+    return 0;
+}
+
+void mover(int posicion){
+    if((posX + posicion >= 0) && (posX + posicion <= maxX-5)) // limita el movimiento del mono para que no se salga del borde 
+        posX = posX + posicion; // Mueve la posicion en n espacios dados
+}
+
+void dibujar(){
+    /* Pintura del monito */
+    attron(COLOR_PAIR(2));    
+    move(posY, posX);
+    printw("  O");
+
+    refresh();
+}
+
